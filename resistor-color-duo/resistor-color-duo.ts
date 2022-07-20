e552a009eb92349c83a9bdbf3e8e543cf09d65a7
crabbit-git <98990717+crabbit-git@users.noreply.github.com>
2022-07-20 15:00:17 +0100

    Resistor Color Duo solved

--- resistor-color-duo/resistor-color-duo.ts
+++ resistor-color-duo/resistor-color-duo.ts
@@ -1,3 +1,22 @@
-export function decodedValue() {
-  throw new Error('Remove this statement and implement this function')
+export const decodedValue = (colors: string[]): number => {
+  const colorKey = [
+    'black',
+    'brown',
+    'red',
+    'orange',
+    'yellow',
+    'green',
+    'blue',
+    'violet',
+    'grey',
+    'white'
+  ];
+  let resistance: string = "";
+  if (!colorKey.includes(colors[0]) || !colorKey.includes(colors[1])) {
+    throw new Error("Invalid input");
+  }
+  for (const color of colors) {
+    resistance += colorKey.indexOf(color).toString();
+  }
+  return parseInt(resistance.substring(0, 2));
 }
